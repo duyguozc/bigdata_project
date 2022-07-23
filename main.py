4024f77ac43a6dd9ee80b81e4687881f8f3006bb
Username <duyguozcelik89@gmail.com>
2022-07-23 16:51:30 -0400

    add new booking and payment record

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import book
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("test")
+    tour_labels = ["T100","T200","T300"]
+    user_id = 1
+    book.book_tour(tour_labels, user_id)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
