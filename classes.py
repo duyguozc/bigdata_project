ecd80939d11e40877b2bc039dfde568d4c44d0aa
Username <duyguozcelik89@gmail.com>
2022-07-24 23:05:07 -0400

    edit a booking, list my bookings

--- classes.py
+++ classes.py
@@ -1,11 +1,21 @@
 class Tour:
-  def __init__(self, id, label, st_date, end_date, total_seats, avail_seats, dest, fare, desc):
-    self.id = id
-    self.label = label
-    self.st_date = st_date
-    self.end_date = end_date
-    self.total_seats = total_seats
-    self.avail_seats = avail_seats
-    self.dest = dest
-    self.fare = fare
-    self.desc = desc
+  def __init__(self, tour_tuple):
+    self.id = tour_tuple[0]
+    self.label = tour_tuple[1]
+    self.st_date = tour_tuple[2]
+    self.end_date = tour_tuple[3]
+    self.total_seats = tour_tuple[4]
+    self.avail_seats = tour_tuple[5]
+    self.dest = tour_tuple[6]
+    self.fare = tour_tuple[7]
+    self.desc = tour_tuple[8]
+
+
+class Booking:
+  def __init__(self, booking_tuple):
+    self.id = booking_tuple[0]
+    self.tour_id = booking_tuple[1]
+    self.user_id = booking_tuple[2]
+    self.number_of_people = booking_tuple[3]
+    self.total_price = booking_tuple[4]
+
--- main.py
+++ main.py
@@ -1,12 +1,82 @@
 import book
+import sqlite3
+from classes import Tour
+from classes import Booking
 
+def get_all_tours():
+    connection = sqlite3.connect('agency_database.db')
+    cursor = connection.cursor()
+    sql = 'SELECT * FROM Tour'
+    cursor.execute(sql)
+    tours = cursor.fetchall()
+    tour_objects = []
+    for t in tours:
+        tour = Tour(t)
+        tour_objects.append(tour)
+    connection.close()
+    return tour_objects
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("test")
-    tour_labels = ["T100","T200","T300"]
-    user_id = 1
-    book.book_tour(tour_labels, user_id)
+    tour_labels = []
+    all_tours = get_all_tours()
+    for row in all_tours:
+        tour_labels.append(row.label)
+
+    print("       Customer Menu        ")
+    print("############################")
+    print("1. My Account")
+    print("2. Search a Tour")
+    print("3. Book a Tour")
+    print("4. My Bookings")
+    print("5. Quit")
+    print("############################")
+    error = True
+    menu = 0
+    while error:
+        try:
+            menu = input("Select a number from the menu: ")
+            menu = int(menu)
+        except ValueError:
+            print("You should enter a number!")
+            continue
+        else:
+            if menu < 1 or menu > 5:
+                print("You should enter a number between 1 and 5!")
+            else:
+                error = False
+    if menu == 3:
+        user_id = 1
+        book.book_tour(tour_labels, user_id)
+    elif menu == 4:
+        all_bookings = book.get_booking_list_of_user(1)
+        index = 1;
+        for book_item in all_bookings:
+            tour = book.get_tour_by_tour_id(book_item.tour_id)
+            print("     (",index,")   ")
+            print("   TOUR INFO    ")
+            book.print_tour(Tour(tour))
+            print("     BOOK INFO      ")
+            print("Number of people to attend: ", book_item.number_of_people)
+            print("Total price: ", book_item.total_price)
+            print()
+            index = index + 1
+        option = int(input("Select a booking number to edit or delete: "))
+
+        selected_book = all_bookings[option - 1]
+        edit_or_del = input("Do you want to edit or delete ? (e/d)")
+        if edit_or_del.lower() == 'e':
+            book.edit_booking(selected_book.id)
+        elif edit_or_del == 'd':
+            delete = input("Are you sure? (y/n)")
+            if delete.lower() == 'y':
+                book.delete_booking(selected_book.id)
+
+
+
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
